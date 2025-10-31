b970396081ff8246f8bd7863f9d763c2cfac8229
Edwin <edwin.sanabria8808@gmail.com>
2025-10-30 20:46:10 -0500

    Cambio de Interfaz activity_home, creacion activity y layout register y password. Creacion de estilos

--- app/src/main/java/com/example/luxury_wash/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/luxury_wash/Activities/HomeActivity.kt
@@ -1,13 +1,30 @@
 package com.example.luxury_wash.Activities
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.luxury_wash.R
+import android.content.Intent
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val tvAbrirReg = findViewById<TextView>(R.id.tv_registrarse)
+        val tvAbrirRecPass = findViewById<TextView>(R.id.tv_recuperarPass)
+
+        tvAbrirReg.setOnClickListener{
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        tvAbrirRecPass.setOnClickListener{
+            val intent = Intent(this,PasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
